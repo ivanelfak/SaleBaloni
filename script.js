873328fa4ac7fee5d2de7d7970ac49875d524474
ivanelfak <ivan.todorovic.m@gmail.com>
2020-01-07 15:39:47 +0100

    7.1 I

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     $('#toggle-btn').click(function () {
-        $('.nav-links').slideToggle(3000);
+        $('.nav-links').slideToggle(2000);
     })
 
-    $(window).scroll(function () {
-        let scroll = $(window).scrollTop();
-        if (scroll >= 70) {
-            $('#nav').addClass("new-nav");
-        }
-        else {
-            $('#nav').removeClass("new-nav");
-        }
-    })
+    //$(window).scroll(function () {
+        //let scroll = $(window).scrollTop();
+       // if (scroll >= 70) {
+       //     $('#nav').addClass("new-nav");
+       // }
+      // else {
+      //      $('#nav').removeClass("new-nav");
+      //  }
+   // })
 
     $('nav-links a').click(function (link) {
 
